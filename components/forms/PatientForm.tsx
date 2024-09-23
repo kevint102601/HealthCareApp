f323edbd5ed0d1832a5793bc3e9f86ab0ff61bac
Kevin Tran <kevint102601@gmail.com>
2024-09-23 15:12:34 -0700

    fixed bugs

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,6 +6,10 @@ import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -17,26 +21,36 @@ export enum FormFieldType {
     SKELETON = 'skeleton',
 }
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
+
 
 const PatientForm = () => {
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const router = useRouter();
+    const [isLoading, setisLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
         defaultValues: {
-            username: "",
+            name: "",
+            email: "",
+            phone: ","
         },
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setisLoading(true);
+
+        try {
+            // const userData = { name, email, phone };
+
+            // const user = await createUser(userData);
+
+            // if (user) router.push(`patients/${user.id}/register)`)
+
+        } catch (error) {
+            console.log(error);
+        }
+
     }
     return (
         <Form {...form}>
@@ -74,7 +88,7 @@ const PatientForm = () => {
                     placeholder="(555) 123-4567"
                 />
 
-                {/* <SubmitButton isLoading={isLoading}>Get Started</SubmitButton> */}
+                <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
             </form>
         </Form>
     )
